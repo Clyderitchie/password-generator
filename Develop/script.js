cf0252b9c3ddadc2180ba231c50e591aa350a5d4
francis <clyderitchie@yahoo.com>
2023-09-02 20:46:22 -0700

    added some if else if and an else statement for confirm windows

--- Develop/script.js
+++ Develop/script.js
@@ -16,18 +16,35 @@ function writePassword() {
 
 }
 
-function getRandomNum(){
-  var min = Math.floor(8);
-  var max = Math.ceil(128);
-  return Math.floor(Math.random * (max - min + 1) + min );
-}
-getRandomNum();
+// function getRandomNum(){
+//   var min = Math.floor(8);
+//   var max = Math.ceil(128);
+//   return Math.floor(Math.random * (max - min + 1) + min );
+// }
+// getRandomNum();
 
 function generatePassword() {
-  var userChoice = prompt('Choose a password length between 8 and 128 characters: ');
+  // var userChoice = prompt('Choose a password length between 8 and 128 characters: ' );
+  var includeNum = window.confirm('Would you like to have numbers in your password');
+  var includeLower = window.confirm('Would you like any lower case letters in your password?');
+  var includeUpper = window.confirm('Would you like any upper case letters in your password?');
+  var includeSpeical = window.confirm('Would you like any special characters in your password?');
+  if (userChoice === includeNum) {
+    newPassword += num;
+  } else if (userChoice === includeLower) {
+    newPassword += low
+  } else if (userChoice === includeUpper) {
+    newPassword += upper
+  } else if (userChoice === includeSpeical) {
+    newPassword += speical
+  } else {
+    newPassword = []
+  }
   
 }
 
 generatePassword();
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
